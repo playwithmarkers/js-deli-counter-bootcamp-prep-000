2b09f74c94849cec01c2489cf7bd045d78c108f1
Benjamin Connor <bsconnor@gmail.com>
2019-12-18 23:21:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function takeANumber(lineOfPeople, newName) {
   lineOfPeople.push(newName);
-  return `Welcome, ${newName}. You are number ${lineOfPeople.length + 1} in line.`;
+  return `Welcome, ${newName}. You are number ${lineOfPeople.length} in line.`;
 }
 
 //takeANumber(katzDeliLine, 'Ada');
